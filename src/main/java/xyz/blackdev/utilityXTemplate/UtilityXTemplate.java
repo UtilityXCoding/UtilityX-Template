0516a5158a6944abe2e11572b6ec960a0496ce15
BlackDev <tjorgefaerber@outlook.com>
2025-07-27 16:24:54 +0200

    initialize UtilityX-Template project with Gradle setup and basic plugin structure

---
+++ src/main/java/xyz/blackdev/utilityXTemplate/UtilityXTemplate.java
@@ -0,0 +1,17 @@
+package xyz.blackdev.utilityXTemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UtilityXTemplate extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
